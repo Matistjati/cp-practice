85d79be3703bea32746928bd91efcb506494b61a
matistjati <matistjati@outlook.com>
2024-03-02 09:44:36 +0100

    Updated statement

--- problems/rangesqum/rangesum/data/gen.sh
+++ problems/rangesqum/rangesum/data/gen.sh
@@ -10,7 +10,7 @@ samplegroup
 limits maxn=100 maxq=100
 sample 1
 
-group naive 50
+group g1 50
 limits maxn=1000 maxq=1000
 include_group sample
 tc g1_1 gen_rand n=1 q=1000
@@ -20,8 +20,8 @@ tc g1_4 gen_rand n=3 q=1000
 tc g1_5 gen_rand n=1000 q=1000
 tc g1_6 gen_rand n=1000 q=1000
 
-group full 50
+group g2 50
 limits maxn=300000 maxq=100000
-include_group naive
+include_group g1
 tc g2_1 gen_rand n=300000 q=100000
 tc g2_2 gen_rand n=300000 q=100000 t=1
