f0d4a4730342f43a713963f3b952e614f366f741
Matistjati <matistjati@outlook.com>
2026-04-14 09:55:57 +0000

    [subsetsum4] avx2 mitigation

--- problems/subsetsum/subsetsum4/submissions/time_limit_exceeded/joshua_bitset.cpp
+++ problems/subsetsum/subsetsum4/submissions/time_limit_exceeded/joshua_bitset.cpp
@@ -1,7 +1,7 @@
 #undef _GLIBCXX_DEBUG                // disable run-time bound checking, etc
 #pragma GCC optimize("Ofast,inline") // Ofast = O3,fast-math,allow-store-data-races,no-protect-parens
 #pragma GCC optimize ("unroll-loops")
-
+#include <bits/allocator.h>
 #pragma GCC target("bmi,bmi2,lzcnt,popcnt")                      // bit manipulation
 #pragma GCC target("movbe")                                      // byte swap
 #pragma GCC target("aes,pclmul,rdrnd")                           // encryption
